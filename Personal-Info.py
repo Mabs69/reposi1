f5f5db3443eb458056eab03ff8b723b3defa40fe
Mabs <82501170+Mabs69@users.noreply.github.com>
2021-04-28 09:26:26 +0800

    Add files via upload

---
+++ Personal-Info.py
@@ -0,0 +1,9 @@
+name = 'Maverick B. Guarismo'
+age = '20'
+year = '2nd Year College Student'
+course = 'Bachelor of Science in Information Technology'
+school = 'De La Salle University-Dasmarinas'
+
+print("Hi! My name is "+name+
+      "\nI am "+age+" Years Old"+". I'm a "+year+" from\n"+school+
+      "\nand currently taking "+course)
